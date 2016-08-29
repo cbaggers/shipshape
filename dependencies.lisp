d3649519304551a479c93103cb207d35c3c76f0c
Baggers <chris.bagley@gmail.com>
2016-08-29 22:05:55 +0200

    use mapcat instead of mapcan

--- dependencies.lisp
+++ dependencies.lisp
@@ -1,12 +1,15 @@
 (in-package :shipshape)
 
+(defun mapcat (function &rest lists)
+  (reduce #'append (apply #'mapcar function lists) :initial-value nil))
+
 (defun walk-dependencies (system function &key flat)
   (let* ((system (etypecase system
 		   (asdf:system system)
 		   ((or string symbol) (asdf:find-system system))))
 	 (depends-on (asdf:system-depends-on system)))
     (remove nil
-	    (funcall (if flat #'mapcan #'mapcar)
+	    (funcall (if flat #'mapcat #'mapcar)
 		     (lambda (x)
 		       (remove nil
 			       (cons (funcall function x)
